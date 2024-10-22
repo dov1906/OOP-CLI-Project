96d26f6eece11945faee4cc77fb87a8bdec553ba
Dov Azogui <dov.azogui@gmail.com>
2024-10-22 12:01:47 -0400

    updated the portfolio class  model

--- lib/models/trader.py
+++ lib/models/trader.py
@@ -3,3 +3,88 @@ from models.__init__ import CONN, CURSOR
 class Trader:
     all = []
     
+    def __init__(self, name):
+        self.name = name
+        self.id = None
+        self.portfolios = []  # Each trader has multiple portfolios
+        Trader.all.append(self)
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if type(value) == str:
+            self._name = value
+        else:
+            raise TypeError("Name must be a string!")
+        
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS traders (
+                id INTEGER PRIMARY KEY,
+                name TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS traders
+        """
+        CURSOR.execute(sql)
+    
+    
+    @classmethod
+    def instance_from_db(cls, row):
+        trader = cls(row[1])
+        trader.id = row[0]
+        return trader
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM traders WHERE id = ?
+        """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT * FROM traders
+        """
+        rows = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.instance_from_db(row) for row in rows]
+        return cls.all
+    
+    def save(self):
+        sql = """
+            INSERT INTO traders (name) VALUES (?)
+        """
+        CURSOR.execute(sql, (self.name,))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        Trader.all.append(self)
+
+
+    @classmethod
+    def create(cls, name):
+        trader = cls(name)
+        trader.save()
+        return trader
+    
+    def delete(self):
+        sql = """
+            DELETE FROM traders WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        Trader.all = [trader for trader in Trader.all if trader.id != self.id]
+
+    def __repr__(self):
+        return f"<Trader {self.name}>"
